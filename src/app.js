9f4e28676acdcfead80ff82d261d32459f700f3f
Dund0u <112277163+Dund0u@users.noreply.github.com>
2023-04-11 16:28:55 +0530

    modified:   src/app.js 	new file:   uploads/hello.txt

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const multer = require('multer');
 const upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, './Back-End/uploads/')
+            cb(null, './uploads/')
         },
         filename: function (req, file, cb) {
             cb(null, file.originalname)
