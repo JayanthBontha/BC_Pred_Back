974cdc57710f62835d39afc84a327bd25897265f
Dund0u <112277163+Dund0u@users.noreply.github.com>
2023-04-11 23:22:35 +0530

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -60,6 +60,9 @@ let transporter = nodemailer.createTransport({
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
