a540f0083ad38137dbd887c5dc8d592d2979ad54
Dund0u <112277163+Dund0u@users.noreply.github.com>
2023-07-07 11:44:56 +0530

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ const upload2 = multer();
 
 app.use(parser.json());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://front-w3hi.onrender.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
