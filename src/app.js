e50a47140da64be6caff516be6dc9f091d34f21a
Dund0u <112277163+Dund0u@users.noreply.github.com>
2023-07-07 12:20:02 +0530

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require('path')
 const cors = require('cors');
 const parser = require('body-parser');
 const app = express();
-app.options('*', cors())
+app.options('https://front-w3hi.onrender.com', cors())
 app.use(cors({
     origin: ['https://front-w3hi.onrender.com']
 }))
